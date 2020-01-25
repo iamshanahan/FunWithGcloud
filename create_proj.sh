a6f6bb4734b54c79843ddc66461f50aa429a2cde
Bill Shanahan <iamshanahan@gmail.com>
2020-01-25 13:07:13 -0800

    First commit: Steps to init and explore project and app-engine app

---
+++ create_proj.sh
@@ -0,0 +1,12 @@
+mkdir mygclapp
+cd mygclapp/
+gcloud projects create llibrary
+gcloud projects list
+gcloud projects describe 838460373880
+gcloud config list
+gcloud config set project llibrary
+gcloud projects delete llibrary
+gcloud projects undelete llibrary
+echo '(Select us west as region)'
+gcloud app create
+
